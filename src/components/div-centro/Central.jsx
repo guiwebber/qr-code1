eac398a3755edc1daff8a2a87b83418f7494827f
Guilherme Webber <guiz1n.webber@gmail.com>
2023-04-06 18:32:58 -0300

    Corrigindo

--- src/components/div-centro/Central.jsx
+++ src/components/div-centro/Central.jsx
@@ -4,9 +4,9 @@ function Central() {
   return (
     <div className="container">
       <div className="div-centro">
-        <img src="../../src/images/favicon-32x32.png" alt="" />
+        <img src="../../src/images/image-qr-code.png" alt="" />
         <div className="text">
-          <h2>Iasasasts</h2>
+          <h2>Improve your front-end skills by building projects</h2>
           <p>
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level.
