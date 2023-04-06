a88d92e8677992ffdf3d20a46269f8779a9653fe
Guilherme Webber <guiz1n.webber@gmail.com>
2023-04-06 18:07:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import "./components/div-centro/Central"
+import Central from "./components/div-centro/Central";
+function App() {
+  return <div className="App">
+    <Central/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/div-centro/Central.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Central.css";
+function Central() {
+  return (
+    <div className="container">
+      <div className="div-centro">
+        <img src="../../src/images/image-qr-code.png" alt="" />
+        <div className="text">
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Central;
