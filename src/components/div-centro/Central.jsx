2e4728a879f03c6237a87df1bc6ff1543de17b40
Guilherme Webber <guiz1n.webber@gmail.com>
2023-04-06 18:31:00 -0300

    Segundo commit

--- src/components/div-centro/Central.jsx
+++ src/components/div-centro/Central.jsx
@@ -6,7 +6,7 @@ function Central() {
       <div className="div-centro">
         <img src="../../src/images/favicon-32x32.png" alt="" />
         <div className="text">
-          <h2>Improve your front-end skills by building projects</h2>
+          <h2>Iasasasts</h2>
           <p>
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level.
